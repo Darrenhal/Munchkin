8dce48d5b37ec7733cddf2b63899ece453580285
Darrenhal <nicolaneher@kabelmail.de>
2021-01-07 20:19:15 +0100

    Implemented displaying munchkin logo in the launcher

--- src/de/munchkin/frontend/MunchkinLauncher.java
+++ src/de/munchkin/frontend/MunchkinLauncher.java
@@ -6,6 +6,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,76 +14,65 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import de.munchkin.utilities.JPictureBox;
+
 public class MunchkinLauncher extends JFrame{
 
 	private static final long serialVersionUID = 1598309638008663371L;
 	
-	private Toolkit toolkit = Toolkit.getDefaultToolkit();
-	private Dimension screenDim = toolkit.getScreenSize();
-	private JPanel contentPane;
-	private JButton btnHostGame, btnJoinGame;
-	private JLabel imageLabel;
-	
+	private final Toolkit toolkit = Toolkit.getDefaultToolkit();
+	private final Dimension screenDim = toolkit.getScreenSize();
+	private final JPanel contentPane = new JPanel(null);
+	private final JButton btnHostGame = new JButton("Host Game");
+	private final JButton btnJoinGame = new JButton("Join Game");
+	private final JPictureBox imageLabel = new JPictureBox();
+	private final Image backgroundImage;
 	
 	public static void main(String[] args) {
 		new MunchkinLauncher();
 	}
 	
 	public MunchkinLauncher() {
-		setTitle("Munchkin Launcher");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Munchkin Launcher");
 		setLocation(screenDim.width / 4, screenDim.height / 4);
 		setSize(screenDim.width / 2, screenDim.height / 2);
 		setMinimumSize(new Dimension(700, 387));
-		
-		contentPane = new JPanel(null);
-		contentPane.setLayout(null);
 		setLayout(null);
-		setContentPane(contentPane);
+		
+		contentPane.setLayout(null);
 		contentPane.setBackground(new Color(253, 205, 136));
+		setContentPane(contentPane);
 		
 		setVisible(true);
 		
-		loadComponents();
+		backgroundImage = loadBackgroundImage();
+		imageLabel.setImage(backgroundImage);
+		
 		loadBounds();
 		addActionListeners();
 		
-		addComponentListener(new ComponentAdapter() {
-			
-			@Override
-			public void componentResized(ComponentEvent e) {
-				super.componentResized(e);
-				loadBounds();
-				repaint();
-			}
-			
-		});
-		
 	}
 	
-	private void loadComponents() {
-		
-		// Load header image
-//		ImageIcon image = new ImageIcon(toolkit.getImage(MunchkinLauncher.class.getResource("Munchkin_logo.jpg")));
-//		
-//		
-//		
-//		imageLabel = new JLabel();
-//		imageLabel.setIcon(image);
-		
-		btnHostGame = new JButton("Host Game");
-		
-		btnJoinGame = new JButton("Join Game");
-		
+	private Image loadBackgroundImage() {
+        final long nowMs = System.currentTimeMillis();
+        final URL res = this.getClass().getResource("/Munchkin_logo.jpg");
+
+        // load Image
+        //      final ImageIcon ret1 = new ImageIcon(res); // is not as good, we eventually only need an Image, we do not need the wrapper that ImageIcon provides
+        //      final BufferedImage ret2 = ImageIO.read(res); // is better, but gives us BufferedImage, which is also more than we need. plus throws IOException
+        final Image ret3 = Toolkit.getDefaultToolkit().getImage(res); // best way. If behaviour plays a role: Loads the image the same way that ImageIcon CTOR would.
+
+        final long durMs = System.currentTimeMillis() - nowMs;
+        System.out.println("Loading BG Image took " + durMs + "ms.");
+
+        return ret3;
 	}
 	
-	private void loadBounds() {
+	void loadBounds() {
 		
 		int width = contentPane.getSize().width;
 		int height = contentPane.getSize().height;
-		
-//		imageLabel.setBounds(20, 20, 700, 210);
-//		contentPane.add(imageLabel);
 		
 		btnHostGame.setBounds(width/2 - 300, height * 2 / 3, 250, 100);
 		contentPane.add(btnHostGame);
@@ -90,9 +80,26 @@ public class MunchkinLauncher extends JFrame{
 		btnJoinGame.setBounds(width/2 + 50, height * 2 / 3, 250, 100);
 		contentPane.add(btnJoinGame);
 		
+		
+		imageLabel.setBounds(20, 0, width - 40, height * 2 / 3);
+		contentPane.add(imageLabel);
+		
+		revalidate();
+		repaint();
+		
 	}
 	
 	private void addActionListeners() {
+		
+		addComponentListener(new ComponentAdapter() {
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				super.componentResized(e);
+				loadBounds();
+			}
+			
+		});
 		
 		btnHostGame.addActionListener(e -> {
 			new MatchCreation();
--- src/de/munchkin/utilities/JPictureBox.java
+++ src/de/munchkin/utilities/JPictureBox.java
@@ -1,83 +1,43 @@
 package de.munchkin.utilities;
 
-import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JComponent;
+public class JPictureBox extends JPanel{
+	
+    private static final long serialVersionUID = 6196514831308649310L;
 
-public class JPictureBox extends JComponent{
 
-    private Icon icon = null;
-    private final Dimension dimension = new Dimension(100, 100);
-    private Image image = null;
-    private ImageIcon ii = null;
-    private SizeMode sizeMode = SizeMode.STRETCH;
-    private int newHeight, newWidth, originalHeight, originalWidth;
 
-    public JPictureBox() {
-        JPictureBox.this.setPreferredSize(dimension);
-        JPictureBox.this.setOpaque(false);
-        JPictureBox.this.setSizeMode(SizeMode.STRETCH);
+    private Image mImage;
+
+    public JPictureBox(final Image pImage) {
+        mImage = pImage;
+    }
+    
+    public JPictureBox() {}
+
+
+    public Image getImage() {
+        return mImage;
+    }
+    public void setImage(final Image pImage) {
+        mImage = pImage;
+        repaint();
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        if (ii != null) {
-            switch (getSizeMode()) {
-                case NORMAL:
-                    g.drawImage(image, 0, 0, ii.getIconWidth(), ii.getIconHeight(), null);
-                    break;
-                case ZOOM:
-                    aspectRatio();
-                    g.drawImage(image, 0, 0, newWidth, newHeight, null);
-                    break;
-                case STRETCH:
-                    g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
-                    break;
-                case CENTER:
-                    g.drawImage(image, (int) (this.getWidth() / 2) - (int) (ii.getIconWidth() / 2), (int) (this.getHeight() / 2) - (int) (ii.getIconHeight() / 2), ii.getIconWidth(), ii.getIconHeight(), null);
-                    break;
-                default:
-                    g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
-            }
-        }
-    }
 
-    public Icon getIcon() {
-        return icon;
-    }
 
-    public void setIcon(Icon icon) {
-        this.icon = icon;
-        ii = (ImageIcon) icon;
-        image = ii.getImage();
-        originalHeight = ii.getIconHeight();
-        originalWidth = ii.getIconWidth();
-    }
+    @Override protected void paintComponent(final Graphics pG) {
+        if (mImage == null) return;
 
-    public SizeMode getSizeMode() {
-        return sizeMode;
-    }
-
-    public void setSizeMode(SizeMode sizeMode) {
-        this.sizeMode = sizeMode;
-    }
-
-    public enum SizeMode {
-        NORMAL,
-        STRETCH,
-        CENTER,
-        ZOOM
-    }
-
-    private void aspectRatio() {
-        if (ii != null) {
-            newHeight = this.getHeight();
-            newWidth = (originalWidth * newHeight) / originalHeight;
-        }
+        final Graphics2D g = (Graphics2D) pG;
+        g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);// can add more
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.drawImage(mImage, 0, 0, getWidth(), getHeight(), this);
     }
 	
 }
